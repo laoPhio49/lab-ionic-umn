e24f17988d7eb2bb1a62132074c96eae595c38c0
Vionie Laorensa <39825885+laoPhio49@users.noreply.github.com>
2019-09-17 21:46:20 +0700

    update booking

--- Week05/src/app/bookings/bookings.page.ts
+++ Week05/src/app/bookings/bookings.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BookingService } from './booking.service';
+import { Router } from '@angular/router';
+import { Booking } from './booking.model';
+import { IonItemSliding } from '@ionic/angular';
 
 @Component({
   selector: 'app-bookings',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bookings.page.scss'],
 })
 export class BookingsPage implements OnInit {
+  bookings: Booking[];
 
-  constructor() { }
+  constructor(
+    private bookingService: BookingService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter(){
+    this.bookings = this.bookingService.getAllBooking();
+  }
+
+  deleteBooking(id: String, slidingItem: IonItemSliding){
+    slidingItem.close();
+    this.bookings = this.bookingService.deleteBooking(id);
   }
 
 }
