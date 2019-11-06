7ec719f29d969136f0bd3e6c319cc0bdd0e54059
LABC705-14 <LABC705-14@EXAM.UMN.AC.ID>
2019-11-06 09:27:18 +0700

    week 12

---
+++ Week12/src/app/auth/auth.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { BehaviorSubject } from 'rxjs';
+import { User } from './user.model';
+import { tap, map } from 'rxjs/operators';
+
+interface AuthResponseData{
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string,
+  localId: string;
+  expiresIn: string;
+  registered?: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  userId = null;
+  private _user = new BehaviorSubject<User>(null);
+
+  constructor(private http: HttpClient) { }
+
+  get isAuthenticated(){
+    return this._user.asObservable().pipe(map(user => {
+      if(user){
+        return !!user.token;
+      } else {
+        return null;
+      }
+    }))
+  }
+
+  signup(email: string, password: string) {
+    return this.http.post<AuthResponseData>
+    (`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseAPIKey}`,
+    {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    }).pipe(
+      tap(userData => {
+        const expTime = new Date(new Date().getTime() + (+userData.expiresIn * 1000));
+        this._user.next(new User(userData.localId, userData.email, userData.idToken, expTime));
+      })
+    );
+  }
+
+  setUser(userId: string){
+    this.userId = userId;
+    // this.isAuthenticated = true;
+  }
+
+  login(email: string, password: string) {
+    //firebase login API here
+    return this.http.post<AuthResponseData>
+      (`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseAPIKey}`,
+      {
+        email,
+        password,
+        returnSecureToken: true,
+      });
+    // this.isAuthenticated = true;
+    // console.log(this.isAuthenticated);
+  }
+
+  logout() {
+    //firebase logout API here
+    // this.isAuthenticated = false;
+    this._user.next(null);
+  }
+
+  
+}
